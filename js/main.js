937afdfbebd3dce9816312316904b4e939e9fc18
DannyPoizot <poizot.dan@gmail.com>
2022-01-09 20:42:42 +0100

    js class init

---
+++ js/main.js
@@ -0,0 +1,33 @@
+"use strict";
+
+const sectionStart = document.querySelector(".sectionStart");
+const sectionGame = document.querySelector(".sectionGame");
+const progressPlayer = document.querySelector('.progressPlayer');
+const progressMonster = document.querySelector('.progressMonster');
+const pvPlayer = document.querySelector('.pvPlayer');
+const pvMonster = document.querySelector('.pvMonster');
+const btnStart = document.getElementById("btn_start");
+const btnAttack = document.getElementById("btn_attack");
+const btnSpecialAttack = document.getElementById("btn_special");
+const btnHeal = document.getElementById("btn_heal");
+const btnGiveUp = document.getElementById("btn_giveup");
+
+let pvP = 100;
+let pvM = 100;
+
+pvPlayer.textContent = pvP;
+pvMonster.textContent = pvM;
+
+sectionGame.hidden = true;
+
+class Character {
+    
+    constructor(name, pv, damage) {
+        this.name = name;
+        this.pv = pv;
+
+    }
+
+
+}
+
